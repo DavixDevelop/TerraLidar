2547bd2f27c0010cb89df28e1d97acc28ccdde7d
David Rupnik <davidrupnik23@gmail.com>
2021-04-28 16:41:00 +0200

    Merged recursive convert_dem with regular convert_dem and added script for converting xyz or acs file containing a TIN surface

---
+++ convert_TIN_to_raster.py
@@ -0,0 +1,181 @@
+import math
+import glob
+import os
+import multiprocessing
+import subprocess
+from osgeo import gdal
+from osgeo import ogr
+from time import sleep
+from qgis.core import QgsProject
+from pathlib import Path
+import concurrent.futures
+import shutil
+from datetime import datetime
+from itertools import repeat
+
+CATEGORY = 'ConvertTINtoRaster'
+source_directory  = "C:/Users/david/Documents/Minecraft/Source" #enter directory with source files
+source_epsg = 'EPSG:5514' #Set the source projection, which will be also used for the output projection, ex. "EPSG:3794". Make sura that you set it to 'EPSG:<YOUR EPSG CODE>'
+source_delimiter = ' ' #Set the delimiter in the xyz or asc file, ex ',' or ';'. Leave at ' ' for space delimiter
+#This script assumes that each row in the source file contains the colums in the next order: x, y, z
+dem_directory = "C:/Users/david/Documents/Minecraft/DEM" #enter directory where you want to save the converted dem files
+
+recursive = False #Set to True if you want the script to also scan for files in sub-folders
+thread_count = None #Set to the number of threads you want to use. Preferably don't use all threads at once. Leave at at None to use all threads
+
+#See https://gdal.org/programs/gdal_grid.html#invdist for explanation
+power = 2.0 #Set power of the invdist algorithm. Leave at 2.0 for default setting
+smoothing = 0.0 #Set the smoothing of the invdist algorithm. Leave at 0.0 for no smoothing
+
+gdal.UseExceptions()
+
+class FileData:
+    def __init__(self, filePath, fileName):
+        self.filePath = filePath
+        self.fileName = fileName
+
+class Job:
+    def __init__(self, powerVal, smoothingVal, sourceEpsg, sourceDel):
+        self.powerVal = powerVal
+        self.smoothingVal = smoothingVal
+        self.sourceEpsg = sourceEpsg
+        self.sourceDel = sourceDel
+
+def processFiles(task):
+    time_start = datetime.now()
+    filesData = []
+    if recursive:
+        #Change the asc or xyz extension, to whatever extension your raster data source uses, but first check if gdal can open it.
+        for raw_file in glob.iglob(source_directory + '**/**', recursive=True):
+            if raw_file.endswith(".asc") or raw_file.endswith(".xyz"):
+                raw_file = raw_file.replace("\\","/")
+                fileinfo = QFileInfo(raw_file)
+                filename = fileinfo.completeBaseName()
+                filesData.append(FileData(raw_file, filename))
+    else:
+        raw_files = os.listdir(source_directory)
+        #Change the asc or xyz extension, to whatever extension your raster data source uses, but first check if gdal can open it.
+        for raw_file in raw_files:
+            if raw_file.endswith(".asc") or raw_file.endswith(".xyz"):
+                fl = os.path.join(source_directory, raw_file)
+                fileinfo = QFileInfo(fl)
+                filename = fileinfo.completeBaseName()
+                filesData.append(FileData(fl, filename))
+
+    if len(filesData) == 0:
+        QgsMessageLog.logMessage(
+                'Found none source files',
+                CATEGORY, Qgis.Info)
+        return None
+
+    QgsMessageLog.logMessage(
+                'Started processing {count} files'.format(count=len(filesData)),
+                CATEGORY, Qgis.Info)
+    cpu_count = 1
+    if thread_count is None:
+        cpu_count = multiprocessing.cpu_count()
+    else:
+        cpu_count = thread_count
+
+    job = Job(power, smoothing, source_epsg, source_delimiter)
+
+    ex = concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count)
+    f = ex.map(processFile, repeat(job), filesData)
+    p = 0
+    result = 0
+    for res in f:
+        if res is not None:
+            result += 1
+        p += 1
+        task.setProgress(int((p * 100) / len(filesData)))
+        sleep(0.05)
+    return [result, time_start]
+
+def filesProccesed(task, result=None):
+    if result is not None:
+        time_end = datetime.now()
+        eclipsed = (time_end - result[1]).total_seconds() / 60.0
+        minutes = math.floor(eclipsed)
+        seconds = math.floor((eclipsed - minutes) * 60)
+        QgsMessageLog.logMessage('Done converting {files_count} files in {minutes} minutes and {seconds} seconds'.format(files_count=result[0], minutes=minutes, seconds=seconds),CATEGORY, Qgis.Info)
+
+def processFile(job_data, file_data):
+    try:
+        #Set output filename
+        tileName = file_data.fileName + ".tif"
+        tile = os.path.join(dem_directory, tileName).replace("\\","/")
+
+        csv_data = []
+
+        #Open source file and create a array representing csv data
+        with open(file_data.filePath, 'r') as dmr_file:
+            for l in dmr_file:
+                if l != "":
+                    xyz_line = l.strip().split(job_data.sourceDel)
+                    csv_data.append("{xc},{yc},{h}\n".format(xc=xyz_line[0],yc=xyz_line[1],h=xyz_line[2]))
+
+        #Check if last row is empty, and remove it
+        if csv_data[len(csv_data) - 1] == "":
+            csv_data.pop()
+
+        #Check if last row ends with new line, trim row
+        lastrow = csv_data[len(csv_data) - 1]
+        if lastrow.endswith('\n'):
+            lastrow = lastrow.rstrip()
+            csv_data[len(csv_data) - 1] = lastrow
+
+        csv_file = os.path.join(dem_directory, file_data.fileName + ".csv").replace("\\","/")
+        if os.path.isfile(csv_file):
+            os.remove(csv_file)
+
+        #Write csv data to file
+        with open(csv_file, 'w') as csf:
+            csf.writelines(csv_data)
+
+
+        #Create array representing lines in a vrt file
+        vrtData = []
+        vrtData.append("<OGRVRTDataSource>\n")
+        vrtData.append('    <OGRVRTLayer name="{name}">\n'.format(name=file_data.fileName))
+        vrtData.append('        <SrcDataSource>{csvfile}</SrcDataSource>\n'.format(csvfile=csv_file))
+        vrtData.append('        <GeometryType>wkbPoint25D</GeometryType>\n')
+        vrtData.append('        <LayerSRS>{sepsg}</LayerSRS>\n'.format(sepsg=job_data.sourceEpsg))
+        vrtData.append('        <GeometryField encoding="PointFromColumns" x="field_1" y="field_2" z="field_3"/>\n')
+        vrtData.append('    </OGRVRTLayer>\n')
+        vrtData.append('</OGRVRTDataSource>')
+
+
+
+        vrt_file = os.path.join(dem_directory, file_data.fileName + ".vrt").replace("\\","/")
+        if os.path.isfile(vrt_file):
+            os.remove(vrt_file)
+
+        #Write vrt to file
+        with open(vrt_file, 'w') as vrf:
+            vrf.writelines(vrtData)
+        sleep(0.01)
+
+        vrts = gdal.OpenEx(vrt_file, 0)
+
+        #Visit https://gdal.org/python/osgeo.gdal-module.html#GridOptions to see other options, like width and height for a higher resolution
+        option = gdal.GridOptions(format='GTiff',outputSRS=job_data.sourceEpsg,algorithm='invdist:power={pow}:smoothing={smo}'.format(pow=job_data.powerVal,smo=job_data.smoothingVal),zfield='field_3')
+        #Interapolate TIN to a grid
+        ds = gdal.Grid(tile,vrts,options=option)
+        ds = None
+        del ds
+
+        vrts = None
+
+        #Remove csv and vrt file
+        os.remove(csv_file)
+        os.remove(vrt_file)
+
+        sleep(0.05)
+        return file_data
+    except Exception as e:
+        QgsMessageLog.logMessage('Error while converting {file}, Error: {er}'.format(file=file_data.fileName, er=str(e)),CATEGORY, Qgis.Info)
+
+    return None
+
+process_task = QgsTask.fromFunction('Converting TIN to raster', processFiles, on_finished=filesProccesed)
+QgsApplication.taskManager().addTask(process_task)
--- convert_dem.py
+++ convert_dem.py
@@ -9,68 +9,104 @@ from qgis.core import QgsProject
 from pathlib import Path
 import concurrent.futures
 import shutil
+from datetime import datetime
+from itertools import repeat
 
 CATEGORY = 'ConvertDem'
-source_directory  = "C:\\Users\\david\\Documents\\Minecraft\\Source" #enter directory with source files
-dem_directory = "C:\\Users\\david\\Documents\\Minecraft\\DEM" #enter directory where you want to save the converted dem files
+#This script assumes that each row in the source file contains the colums in the next order: x, y, z
+source_directory  = "C:/Users/david/Documents/Minecraft/Source" #enter directory with source files
+source_epsg = None #Set the source projection, which will be also used for the output projection, ex. "EPSG:3794". Leave at None to ignore. else make sura that you set it to 'EPSG:<YOUR EPSG CODE>'
+dem_directory = "C:/Users/david/Documents/Minecraft/DEM" #enter directory where you want to save the converted dem files
+
+recursive = False #Set to True if you want the script to also scan for files in sub-folders
+thread_count = None #Set to the number of threads you want to use. Preferably don't use all threads at once. Leave at at None to use all threads
+
+gdal.UseExceptions()
+
+class FileData:
+    def __init__(self, filePath, fileName):
+        self.filePath = filePath
+        self.fileName = fileName
+
+def processFiles(task):
+
+    time_start = datetime.now()
+    filesData = []
+    if recursive:
+        #Change the asc or xyz extension, to whatever extension your raster data source uses, but first check if gdal can open it.
+        for raw_file in glob.iglob(source_directory + '**/**', recursive=True):
+            if raw_file.endswith(".asc") or raw_file.endswith(".xyz"):
+                raw_file = raw_file.replace("\\","/")
+                fileinfo = QFileInfo(raw_file)
+                filename = fileinfo.completeBaseName()
+                filesData.append(FileData(raw_file, filename))
+    else:
+        raw_files = os.listdir(source_directory)
+        #Change the asc or xyz extension, to whatever extension your raster data source uses, but first check if gdal can open it.
+        for raw_file in raw_files:
+            if raw_file.endswith(".asc") or raw_file.endswith(".xyz"):
+                fl = os.path.join(source_directory, raw_file)
+                fileinfo = QFileInfo(fl)
+                filename = fileinfo.completeBaseName()
+                filesData.append(FileData(fl, filename))
+
+    if len(filesData) == 0:
+        QgsMessageLog.logMessage(
+                'Found none source files',
+                CATEGORY, Qgis.Info)
+        return None
 
 
-def processFiles(task, filesData):
-	QgsMessageLog.logMessage(
-				'Started processing {count} files'.format(count=len(filesData)),
-				CATEGORY, Qgis.Info)
-	cpu_count = multiprocessing.cpu_count()
-	cpu_count = min(6, cpu_count)
-	ex = concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count)
-	f = ex.map(processFile, filesData)
-	p = 0
-	result = 0
-	for res in f:
-		if res is not None:
-			QgsMessageLog.logMessage(
-					'Processed file {name}'.format(name=res),
-					CATEGORY, Qgis.Info)
-			result += 1
-		p += 1
-		task.setProgress(int((p * 100) / len(filesData)))
-		sleep(0.05)
-	return result
+    QgsMessageLog.logMessage(
+                'Started processing {count} files'.format(count=len(filesData)),
+                CATEGORY, Qgis.Info)
+    cpu_count = 1
+    if thread_count is None:
+        cpu_count = multiprocessing.cpu_count()
+    else:
+        cpu_count = thread_count
+
+    ex = concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count)
+    f = ex.map(processFile, repeat(source_epsg) filesData)
+    p = 0
+    result = 0
+    for res in f:
+        if res is not None:
+            result += 1
+        p += 1
+        task.setProgress(int((p * 100) / len(filesData)))
+        sleep(0.05)
+    return [result, time_start]
 
 def filesProccesed(task, result=None):
-	if result is not None:
-		QgsMessageLog.logMessage('Done converting {files_count} files'.format(files_count=result),CATEGORY, Qgis.Info)
+    if result is not None:
+        time_end = datetime.now()
+        eclipsed = (time_end - result[1]).total_seconds() / 60.0
+        minutes = math.floor(eclipsed)
+        seconds = math.floor((eclipsed - minutes) * 60)
+        QgsMessageLog.logMessage('Done converting {files_count} files in {minutes} minutes and {seconds} seconds'.format(files_count=result[0], minutes=minutes, seconds=seconds),CATEGORY, Qgis.Info)
 
 
-def processFile(file_data):
-	try:
-		tileName = file_data[1] + ".tif"
-		tile = os.path.join(dem_directory, tileName)
-		ds = gdal.Open(file_data[0])
-		sleep(0.05)
-		#Visit https://gdal.org/python/osgeo.gdal-module.html#TranslateOptions to see other options, like width and height for a higher resolution
-		ds = gdal.Translate(tile,ds,format="GTiff",resampleAlg="cubic")
-		ds = None
-		sleep(0.05)
-		return file_data[1]
-	except Exception as e:
-		QgsMessageLog.logMessage('Error while converting {file}, Error: {er}'.format(file=file_data[1], er=str(e)),CATEGORY, Qgis.Info)
+def processFile(sourceEpsg, file_data):
+    try:
+        tileName = file_data.fileName + ".tif"
+        tile = os.path.join(dem_directory, tileName)
+        ds = gdal.Open(file_data.filePath)
+        sleep(0.05)
+        #Visit https://gdal.org/python/osgeo.gdal-module.html#TranslateOptions to see other options, like width and height for a higher resolution
+        if sourceEpsg is None:
+            ds = gdal.Translate(tile,ds,format="GTiff",resampleAlg="cubic")
+            ds = None
+        else:
+            ds = gdal.Translate(tile,ds,format="GTiff",outputSRS=sourceEpsg,resampleAlg="cubic")
+            ds = None
 
-	return None
+        sleep(0.05)
+        return file_data
+    except Exception as e:
+        QgsMessageLog.logMessage('Error while converting {file}, Error: {er}'.format(file=file_data.fileName, er=str(e)),CATEGORY, Qgis.Info)
 
-raw_files = os.listdir(source_directory)
+    return None
 
-files = []
-
-
-#Change the asc or xyz extension, to whatever extension your raster data source uses, but first check if gdal can open it.
-for raw_file in raw_files:
-	if raw_file.endswith(".asc") or raw_file.endswith(".xyz"):
-		fl = os.path.join(source_directory, raw_file)
-		fileinfo = QFileInfo(fl)
-		filename = fileinfo.completeBaseName()
-		files.append([fl, filename])
-
-QgsMessageLog.logMessage('Found {count} files'.format(count=len(files)),CATEGORY, Qgis.Info)
-
-process_task = QgsTask.fromFunction('Finished converting {0} files'.format(len(files)), processFiles, on_finished=filesProccesed, filesData=files)
+process_task = QgsTask.fromFunction('Converting to DEM'.format(len(files)), processFiles, on_finished=filesProccesed)
 QgsApplication.taskManager().addTask(process_task)
