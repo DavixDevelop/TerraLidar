950d658feb0d948d10318fd94a05de46558f81a9
David Rupnik <davidrupnik23@gmail.com>
2021-08-23 20:17:45 +0200

    Added option to set parameters like dem resolution in export_dem

--- export_dem.py
+++ export_dem.py
@@ -18,18 +18,35 @@ dem_directory = "C:/Users/david/Documents/Minecraft/DEM" #enter directory where 
 cloud_compare_path = "C:\\Program Files\\CloudCompare\\CloudCompare" #Set it to the the path of CloudCompare
 extract_ground = False #Set it to True if your LiDAR data contains both ground and off-ground points (Like trees, buildings...)
 
+#Grid step refers to the grid size of the output raster (the unit is same as the unit of pointclouds). Ex. setting it to 1 would result in a DEM with a resolution of 1 meter
+grid_step = 1
+
 recursive = False #Set to True if you want the script to also scan for files in sub-folders
 thread_count = None #Set to the number of threads you want to use. Preferably don't use all threads at once. Leave at at None to use all threads
 
 default_nodata = -32768 #Don't change the value of nodata, if you don't know it
 
+#QCSF plugin parameters (when extract_ground is set to True)
+#Set the scene of the lidar (SLOPE - Steep slope, RELIEF, FLAT)
+lidar_scene = 'SLOPE'
+#Cloth resolution refers to the grid size (the unit is same as the unit of pointclouds) of cloth which is used to cover the terrain.
+#The bigger cloth resolution you have set, the coarser DTM  you will get. Ex 1.0 would result in a grid size of 1 meter
+cloth_resolution = 0.5
+#Classification threshold refers to a threshold (the unit is same as the unit of pointclouds) to classify the pointclouds into
+#ground and non-ground parts based on the distances between points and the simulated terrain. 0.5 is adapted to most of scenes.
+class_threshold = 0.5
+
 class Job:
-    def __init__(self, srcDir, demDir, extractGround, cloudCompare, defNoData):
+    def __init__(self, srcDir, demDir, extractGround, cloudCompare, defNoData, gridStep, scene, clothResolution, classThreshold):
         self.srcDir = srcDir
         self.demDir = demDir
         self.extractGround = extractGround
         self.cloudCompare = cloudCompare
         self.defNoData = defNoData
+        self.gridStep = gridStep
+        self.scene = scene
+        self.clothResolution = clothResolution
+        self.classThreshold = classThreshold
 
 class FileData:
     def __init__(self, filePath, fileName):
@@ -73,7 +90,7 @@ def processFiles(task):
     else:
         cpu_count = thread_count
 
-    job = Job(source_directory, dem_directory, extract_ground, cloud_compare_path, default_nodata)
+    job = Job(source_directory, dem_directory, extract_ground, cloud_compare_path, default_nodata, grid_step, lidar_scene, cloth_resolution, class_threshold)
 
     ex = concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count)
     f = ex.map(processFile, repeat(job), filesData)
@@ -99,7 +116,7 @@ def processFile(job_data, file_data):
     try:
 
         if job_data.extractGround:
-            subprocess.check_output('\"{cloud}\" -SILENT -O -GLOBAL_SHIFT AUTO  \"{file}\" -CSF -SCENES SLOPE -CLOTH_RESOLUTION 0.5 -CLASS_THRESHOLD 0.5 -RASTERIZE -GRID_STEP 1 -VERT_DIR 2 -PROJ MIN -SF_PROJ AVG -EMPTY_FILL INTERP -OUTPUT_RASTER_Z'.format(cloud=job_data.cloudCompare, file=file_data.filePath), cwd=job_data.srcDir, shell=True)
+            subprocess.check_output('\"{cloud}\" -SILENT -O -GLOBAL_SHIFT AUTO  \"{file}\" -CSF -SCENES {scene} -CLOTH_RESOLUTION {clothResolution} -CLASS_THRESHOLD {classThreshold} -RASTERIZE -GRID_STEP {gridStep} -VERT_DIR 2 -PROJ MIN -SF_PROJ AVG -EMPTY_FILL INTERP -OUTPUT_RASTER_Z'.format(cloud=job_data.cloudCompare, file=file_data.filePath, scene=job_data.scene, clothResolution=str(job_data.clothResolution), classThreshold=str(job_data.classThreshold), gridStep=str(job_data.gridStep)), cwd=job_data.srcDir, shell=True)
             sleep(0.05)
             for old_tif in Path(source_directory).rglob('{filename}_ground_points*.tif'.format(filename=file_data.fileName)):
                 os.rename(old_tif, os.path.join(source_directory, "{filename}.tif".format(filename=file_data.fileName)))
@@ -108,7 +125,7 @@ def processFile(job_data, file_data):
             os.rename(os.path.join(source_directory, "{filename}.tif".format(filename=file_data.fileName)), file_name)
             sleep(0.05)
         else:
-            subprocess.check_output('\"{cloud}\" -SILENT -O -GLOBAL_SHIFT AUTO  \"{file}\" -RASTERIZE -GRID_STEP 1 -VERT_DIR 2 -PROJ MIN -SF_PROJ AVG -EMPTY_FILL INTERP -OUTPUT_RASTER_Z'.format(cloud=job_data.cloudCompare,file=file_data.filePath), cwd=job_data.srcDir, shell=True)
+            subprocess.check_output('\"{cloud}\" -SILENT -O -GLOBAL_SHIFT AUTO  \"{file}\" -RASTERIZE -GRID_STEP {gridStep} -VERT_DIR 2 -PROJ MIN -SF_PROJ AVG -EMPTY_FILL INTERP -OUTPUT_RASTER_Z'.format(cloud=job_data.cloudCompare,file=file_data.filePath, gridStep=str(job_data.gridStep)), cwd=job_data.srcDir, shell=True)
             sleep(0.05)
             for old_tif in Path(source_directory).rglob('{filename}_*.tif'.format(filename=file_data.fileName)):
                 os.rename(old_tif, os.path.join(source_directory, "{filename}.tif".format(filename=file_data.fileName)))
